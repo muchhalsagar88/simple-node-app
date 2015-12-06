c846c49427a454eb2c3cb2559eefbe2e4fba5594
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-12-06 13:58:14 -0500

    Added server logic

---
+++ app.js
@@ -0,0 +1,37 @@
+var http = require('http'),
+	redis = require('redis-connection-pool')('myRedisPool', {
+		host: '127.0.0.1', 
+		port: 6379,  
+		max_clients: 5, 
+		database: 0, // database number to use 
+	});
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var serveRequest = function(){
+
+	var prime_number = 5743,
+		rand_num = getRandomInt(1, 100000000);
+    while(rand_num > 1000000 || rand_num % prime_number != 0)
+        rand_num = getRandomInt(1, 100000000)
+
+    return ''+rand_num; 
+}
+
+var requestHandler = function(req, res) {
+	var hrstart = process.hrtime();
+	res.writeHead(200);
+    res.write(serveRequest());
+    res.end();
+    hrend = process.hrtime(hrstart);
+    redis.lpush('latency', hrend[1]/1000000);
+}
+
+var server = http.createServer(requestHandler);
+ 
+var port = 10001;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
